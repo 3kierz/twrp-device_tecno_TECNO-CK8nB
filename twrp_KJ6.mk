99601a368e116bc71fe1220aa6eda1af3a5255ec
Ethan Kier <3kierz@gmail.com>
2024-09-15 07:07:00 +0800

    Update twrp_KJ6.mk

--- twrp_KJ6.mk
+++ twrp_KJ6.mk
@@ -4,17 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from TECNO-KJ6device
-$(call inherit-product, device/tecno/TECNO-KJ6/device.mk)
+# Inherit from TECNO-CK8NB device
+$(call inherit-product, device/tecno/TECNO-CK8NB/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Product Specifics
-PRODUCT_NAME := twrp_KJ6
-PRODUCT_DEVICE := TECNO-KJ6
+PRODUCT_NAME := twrp_CK8NB
+PRODUCT_DEVICE := TECNO-CK8NB
 PRODUCT_BRAND := TECNO
-PRODUCT_MODEL := TECNO KJ6
+PRODUCT_MODEL := TECNO CK8NB
 PRODUCT_MANUFACTURER := TECNO
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
